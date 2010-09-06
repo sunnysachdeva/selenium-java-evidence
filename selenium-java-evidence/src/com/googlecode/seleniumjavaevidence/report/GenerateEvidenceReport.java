f4a63a9f22bdd4777343a1f8cf6b1f5f815ea02c
elias.nogueira <elias.nogueira@461cd21e-c673-655f-7335-0907f4d48236>
2010-09-06 01:51:57 +0000

    Start subversion

---
+++ selenium-java-evidence/src/com/googlecode/seleniumjavaevidence/report/GenerateEvidenceReport.java
@@ -0,0 +1,110 @@
+package com.googlecode.seleniumjavaevidence.report;
+
+import com.googlecode.seleniumjavaevidence.selenium.SeleniumEvidence;
+import com.googlecode.seleniumjavaevidence.utils.SeleniumEvidenceUtils;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import javax.imageio.ImageIO;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+/**
+ * Generate the test evidence in PDF file
+ * @author Elias Nogueira <elias.nogueira@gmail.com>
+ * @author Sem Bugs <http://sembugs.blogspot.com>
+ */
+public class GenerateEvidenceReport {
+
+    /**
+     * Generate evidence in PDF file. The evidence will be save on home dir on a folder called by the name of the project
+     * @param list list of evidence made of an text and image
+     * @param reportName report name
+     * @param exception exception text that are throwing by Java
+     * @throws IOException if occurs any problem with the directory
+     */
+    public void generatePDFEvidence(List<SeleniumEvidence> list, String reportName, String exception) throws IOException {
+        List<SeleniumEvidence> data = list;
+        Properties properties = SeleniumEvidenceUtils.loadProperties();
+        String project = properties.getProperty("label.projetc");
+        String tester = properties.getProperty("label.tester");
+
+        createDirToEvidence(project);
+
+        String userHome = System.getProperty("user.dir") + System.getProperty("file.separator") + project + System.getProperty("file.separator");
+
+        try {
+            File dir = new File(userHome);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+
+            BufferedImage imageCompany = ImageIO.read(new File(properties.getProperty("image.company.path")));
+            BufferedImage imageClient = ImageIO.read(new File(properties.getProperty("image.customer.path")));
+
+            Map parameters = new HashMap();
+            if (exception != null) {
+                parameters.put("SEL_EXCEPTION", exception);
+            }
+            parameters.put("SEL_COMPANY_LOGO", imageCompany);
+            parameters.put("SEL_CUSTOMER_LOGO", imageClient);
+            parameters.put("SEL_PROJECT", project);
+            parameters.put("SEL_TESTER", tester);
+
+            parameters.put("SEL_LABEL_EVINDENCE_TITLE", properties.getProperty("label.evidenceReport"));
+            parameters.put("SEL_LABEL_PROJECT", properties.getProperty("label.projetc"));
+            parameters.put("SEL_LABEL_TESTER", properties.getProperty("label.tester"));
+            parameters.put("SEL_LABEL_STATUS", properties.getProperty("label.status"));
+            parameters.put("SEL_LABEL_PASS", properties.getProperty("label.status.pass"));
+            parameters.put("SEL_LABEL_FAILED", properties.getProperty("label.status.failed"));
+            parameters.put("SEL_LABEL_EVIDENCE_REPORT", properties.getProperty("label.evidenceReport"));
+            parameters.put("SEL_LABEL_DATE", properties.getProperty("label.date"));
+            parameters.put("SEL_LABEL_FOOTER", properties.getProperty("label.footer"));
+            parameters.put("SEL_LABEL_ERROR_DETAIL", properties.getProperty("label.errorDetail"));
+            parameters.put("SEL_LABEL_PAGE", properties.getProperty("label.page"));
+            parameters.put("SEL_LABEL_COMPANY_NAME", properties.getProperty("label.company.name"));
+
+            JRBeanCollectionDataSource datasource = new JRBeanCollectionDataSource(data);
+            JasperReport jasperReport = JasperCompileManager.compileReport(properties.getProperty("evidence.file"));
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, datasource);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, userHome + reportName + ".pdf");
+
+        } catch (SecurityException ex) {
+            ex.printStackTrace();
+        } catch (JRException jre) {
+            jre.printStackTrace();
+        }
+    }
+
+    /**
+     * Create a directory with the project's name
+     * @param project project name
+     */
+    private boolean createDirToEvidence(String project) {
+        boolean dirExists = false;
+        String userHome = System.getProperty("evidence.dir") + System.getProperty("file.separator") + project;
+        try {
+            File dir = new File(userHome);
+            boolean exists = dir.exists();
+
+            if (!exists) {
+                dir.mkdir();
+                dirExists = false;
+            } else {
+                dirExists = true;
+            }
+        } catch (SecurityException se) {
+            se.printStackTrace();
+        }
+        return dirExists;
+    }
+}
